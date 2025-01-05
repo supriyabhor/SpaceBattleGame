ab7bae02960600ff44edad93d49d72802bae35e4
supriyabhor <supriyanaikwadi89@gmail.com>
2025-01-05 16:40:10 -0500

    created ship class

---
+++ app.js
@@ -0,0 +1,8 @@
+// Ship Class (Base Class)
+class Ship {
+    constructor(hull, firepower, accuracy) {
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+}
